708ad8d76638449d52198082149238ced367fa12
bing <bing1894917@gmail.com>
2017-08-20 12:57:23 +0800

    iii

--- thumb.sh
+++
@@ -1,4 +0,0 @@
-node /Users/tt/ww/node-quickthumb/bin/make-thumb.js ./assets/images/ ./assets/images/thumb 300x200
-node /Users/tt/ww/node-quickthumb/bin/make-thumb.js ./assets/gallery/macro ./assets/gallery/macro/thumb 200x200
-
-node /Users/tt/ww/node-quickthumb/bin/make-thumb.js ./ ./thumb 200x200
